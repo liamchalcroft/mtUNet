Fix classification softmax and squeeze masks for surface dist

nnunetv2/inference/predict_multitask.py
@@ -170,8 +170,13 @@ class MultiTaskPredictor(nnUNetPredictor):
                 prediction = prediction.to('cpu')
                 
                 # Process classifications
-                class_probs = torch.softmax(classifications, dim=0)
-                predicted_class = torch.argmax(class_probs).item()
+                print(f"classifications shape: {classifications.shape}")
+                print(f"classifications: {classifications}")
+                class_probs = torch.softmax(classifications, dim=-1)
+                print(f"class_probs shape: {class_probs.shape}")
+                print(f"class_probs: {class_probs}")
+                predicted_class = torch.argmax(class_probs, dim=-1).item()
+                print(f"predicted_class: {predicted_class}")
                 classifications_dict['Subtype'].append(predicted_class)
 
                 if ofile is not None:

nnunetv2/training/nnUNetTrainer/nnUNetMultiTaskTrainer.py
@@ -1380,8 +1380,8 @@ class nnUNetMultiTaskTrainer(object):
                 class_prediction = class_prediction.cpu()
 
                 # Process classification results
-                class_probs = torch.softmax(class_prediction, dim=0)
-                predicted_class = torch.argmax(class_probs).item()
+                class_probs = torch.softmax(class_prediction, dim=-1)
+                predicted_class = torch.argmax(class_probs, dim=-1).item()
                 classifications_dict['Names'].append(k + '.nii.gz')
                 classifications_dict['Subtype'].append(predicted_class)
 